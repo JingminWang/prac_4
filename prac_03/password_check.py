f58bd90934adc846894bd586d32a388e224e7958
JingminWang <3202241005@stu.xaut.edu.cn>
2021-09-21 22:54:48 +0800

    prints the asterisks into a function

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -3,11 +3,12 @@ def main():
     password = input(">")
     while not is_valid_password(password):
         print("invalid password")
-        for i in range(len(password)):
-            print('*', end='')
+        Print_asterisks(password)
         print()
         password = input(">")
     print("is valid")
+    Print_asterisks(password)
+def Print_asterisks(password):
     for i in range(len(password)):
         print('*', end='')
 def is_valid_password(password):
