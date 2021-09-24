d6347f057f22e19236299da3152166f193027beb
JingminWang <3202241005@stu.xaut.edu.cn>
2021-09-24 09:22:11 +0800

    Put input output outside the function

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -11,20 +11,20 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            Celsius_to_Fahrenheit()
+            celsius = float(input("Celsius: "))
+            Celsius_to_Fahrenheit(celsius)
         elif choice == "F":
-            Fahrenheit_to_Celsius()
+            fahrenheit = float(input("Fahrenheit: "))
+            Fahrenheit_to_Celsius(fahrenheit)
         else:
             print("Invalid option")
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you.")
-def Celsius_to_Fahrenheit():
-    celsius = float(input("Celsius: "))
+def Celsius_to_Fahrenheit(celsius):
     fahrenheit = celsius * 9.0 / 5 + 32
     print("Result: {:.2f} F".format(fahrenheit))
-def Fahrenheit_to_Celsius():
-    fahrenheit = float(input("Fahrenheit: "))
+def Fahrenheit_to_Celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 1.8
     print("Result: {:.2f} C".format(celsius))
 main()
