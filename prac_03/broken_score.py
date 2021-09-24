3dc47bc2e4755b44c3957ec85244f6d05e6fee8e
JingminWang <3202241005@stu.xaut.edu.cn>
2021-09-24 09:29:17 +0800

    Add function and random

---
+++ prac_03/broken_score.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+# TODO: Fix this!
+def main():
+    score = float(input("Enter score: "))
+    print(return_result(score))
+    print()
+    import random
+    score_r=random.randint(1, 100)
+    print(score_r)
+    print(return_result(score_r))
+
+def return_result(score):
+    if score < 0 or score > 100:
+        return("Invalid score")
+    elif score >= 90:
+        return("Excellent")
+    elif score >= 50:
+        return("Passable")
+    else:
+        return("Bad")
+main()
