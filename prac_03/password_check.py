8694f2446d69857416f89654d618112cac9586d3
JingminWang <3202241005@stu.xaut.edu.cn>
2021-09-24 09:06:15 +0800

    Reformat the code

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -7,17 +7,23 @@ def main():
         password = input(">")
     print("is valid")
     Print_asterisks(password)
+
+
 def get_password():
-   return input(">")
+    return input(">")
 
 
 def Print_asterisks(password):
     for i in range(len(password)):
         print('*', end='')
+
+
 def is_valid_password(password):
     Min_length = 5
-    if len(password)<Min_length:
+    if len(password) < Min_length:
         return False
     else:
         return True
+
+
 main()
